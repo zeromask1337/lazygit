849524f8c3243971afa3c02a777ddec4a6631949
Jesse Duffield <jessedduffield@gmail.com>
2023-12-02 12:45:15 +1100

    Layout the bottom line view using spacer views
    We are also removing the single-character padding on the left/right edges of the bottom
    line because it's unnecessary
    
    Unfortunately we need to create views for each spacer: it's not enough to just
    layout the existing views with padding inbetween because gocui only renders
    views meaning if there is no view in a given position, that position will just
    render whatever was there previously (at least that's what I recall from talking
    this through with Stefan: I could be way off).
    
    Co-authored-by: Stefan Haller <stefan@haller-berlin.de>

--- pkg/gui/views.go
+++ pkg/gui/views.go
@@ -55,6 +55,11 @@ func (gui *Gui) orderedViewNameMappings() []viewNameMapping {
 		{viewPtr: &gui.Views.Search, name: "search"},
 		// this view shows either the "Search:" prompt when searching, or the "Filter:" prompt when filtering
 		{viewPtr: &gui.Views.SearchPrefix, name: "searchPrefix"},
+		// these views all contain one space, and are used as spacers before, between, and after the various views in the bottom line
+		{viewPtr: &gui.Views.StatusSpacer1, name: "statusSpacer1"},
+		{viewPtr: &gui.Views.StatusSpacer2, name: "statusSpacer2"},
+		{viewPtr: &gui.Views.StatusSpacer3, name: "statusSpacer3"},
+		{viewPtr: &gui.Views.StatusSpacer4, name: "statusSpacer4"},
 
 		// popups.
 		{viewPtr: &gui.Views.CommitMessage, name: "commitMessage"},
@@ -97,6 +102,11 @@ func (gui *Gui) createAllViews() error {
 	gui.Views.SearchPrefix.FgColor = gocui.ColorCyan
 	gui.Views.SearchPrefix.Frame = false
 	gui.c.SetViewContent(gui.Views.SearchPrefix, gui.Tr.SearchPrefix)
+
+	gui.Views.StatusSpacer1.Frame = false
+	gui.Views.StatusSpacer2.Frame = false
+	gui.Views.StatusSpacer3.Frame = false
+	gui.Views.StatusSpacer4.Frame = false
 
 	gui.Views.Search.BgColor = gocui.ColorDefault
 	gui.Views.Search.FgColor = gocui.ColorCyan
